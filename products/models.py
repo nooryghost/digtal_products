1b0a0c6a27ab4f211f6721bbda861ade10729307
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-02-27 18:25:30 +0330

    models completed

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Category(models.Model):
-    parent = models.ForeignKey("self", verbose_name=_("parent"), blank=True, null=True, on_delete=models.CASCADE())
+    parent = models.ForeignKey("self", verbose_name=_("parent"), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(_("title") ,max_length=50)
     description = models.TextField(_("description") ,blank=True)
     avatar = models.ImageField(_("avatar"), blank=True, upload_to="categories")
@@ -16,8 +16,27 @@ class Category(models.Model):
         verbose_name_plural = _("Categories")
 
 class Product(models.Model):
-    pass
+    title = models.CharField(_("title"), max_length=50)
+    description = models.TextField(_("description"), blank=True)
+    avatar = models.ImageField(_("avatar"), upload_to="products/")
+    categories = models.ManyToManyField("Category", verbose_name=_("categories"), blank=True)
+    created_time = models.DateTimeField(_("created time"), auto_now_add=True)
+    updated_time = models.DateTimeField(_("updated time"), auto_now=True)
 
+    class Meta:
+        db_table = _("products")
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey("Product", verbose_name=_("product"), on_delete=models.CASCADE)
+    title = models.CharField(_("title"), max_length=50)
+    file = models.FileField(_("file"), upload_to="files/%Y/%m/%d/")
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(_("created time"), auto_now_add=True)
+    updated_time = models.DateTimeField(_("updated time"), auto_now=True)
+
+    class Meta:
+        db_table = _("files")
+        verbose_name = _("File")
+        verbose_name_plural = _("Files")
