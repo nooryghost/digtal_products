6409357286feab85797abc16c2625458aaa3e09f
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-02-27 22:40:56 +0330

    Product models added.

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     parent = models.ForeignKey("self", verbose_name=_("parent"), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(_("title") ,max_length=50)
     description = models.TextField(_("description") ,blank=True)
-    avatar = models.ImageField(_("avatar"), blank=True, upload_to="categories")
+    avatar = models.ImageField(_("avatar"), blank=True, upload_to="categories/")
     is_enable = models.BooleanField(_("is enable"), default=True)
     created_time = models.DateTimeField(_("created time"), auto_now_add=True)
     updated_time = models.DateTimeField(_("updated time"), auto_now=True)
