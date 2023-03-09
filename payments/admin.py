24f70ec7aab6a6df26f8854b39c973a0844b5cf1
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-09 12:09:47 +0330

    admin updated

--- payments/admin.py
+++ payments/admin.py
@@ -7,4 +7,6 @@ class GatewayAdmin(admin.ModelAdmin):
     list_display = ["title", "is_enable"]
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ["user"]
+    list_display = ["user", "package", "gateway", "price", "status", "phone_number", "created_time"]
+    list_filter = ["status", "gateways", "packages"]
+    search_fields = ["user__username", "phone_number"]
