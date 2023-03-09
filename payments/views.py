e71244f11edd379ea6b86043fbef6240d954d084
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-09 16:52:31 +0330

    payment view part 2

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,6 @@
 import uuid
+import requests
+from datetime import timezone 
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,7 +9,7 @@ from rest_framework import status
 
 from .models import GateWay, Payment
 from .serializers import GatewaySerializer
-from subscriptions.models import Package
+from subscriptions.models import Package, Subscription
 
 class GatewayView(APIView):
 
@@ -41,3 +43,39 @@ class PaymentView(APIView):
         )
 
         return Response({"token": payment.token, "callback_url": "https://my-site.com/payments/pay/"})
+
+    def post(self, request):
+        token = request.data.get("token")
+        st = request.data.get("status")
+
+        try:
+            payment = Payment.objects.get(token=token)
+
+        except Payment.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if st != 10:
+            payment.status = Payment.STATUS_CANCELED
+            payment.save()
+
+            return Response({"detail": "Payment verification failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
+        r = requests.post("bank_verify_url", data={})
+        if r.status_code // 100 != 2:
+            payment.status = Payment.STATUS_ERROR
+            payment.save()
+
+            return Response({"detail": "Payment verification failed"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
+        payment.status = Payment.STATUS_PAID
+        payment.save()
+
+        Subscription.objects.create(
+            user=payment.user,
+            package=payment.package,
+            expire_time=timezone.now() + timezone.timedelta(days=payment.package.duration.days)
+        )
+
+        return Response()
