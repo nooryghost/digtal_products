b2a417731f03d6e151fb2c425a6b445e45a8320c
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-09 17:46:56 +0330

    fixed little bug

--- payments/admin.py
+++ payments/admin.py
@@ -5,8 +5,9 @@ from .models import GateWay, Payment
 @admin.register(GateWay)
 class GatewayAdmin(admin.ModelAdmin):
     list_display = ["title", "is_enable"]
+
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ["user", "package", "gateway", "price", "status", "phone_number", "created_time"]
-    list_filter = ["status", "gateways", "packages"]
+    list_filter = ["status", "gateway", "package"]
     search_fields = ["user__username", "phone_number"]
