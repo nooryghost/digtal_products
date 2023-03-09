348aa37b24bf38e2c98f6a0ef36b95c6f6268570
nooryghost <109327061+nooryghost@users.noreply.github.com>
2023-03-09 11:57:29 +0330

    updated 2

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from utils.validators import validate_phone_number
+
 class GateWay(models.Model):
     title = models.CharField(_("title"), max_length=50)
     description = models.TextField(_("description"), blank=True)
